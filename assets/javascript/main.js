c58349d780a49509bae6f3b96dcc3d572f1a4371
famouswolfe <famouswolfe@gmail.com>
2017-07-22 18:26:26 -0600

    updates

---
+++ assets/javascript/main.js
@@ -0,0 +1,10 @@
+/*
+on page load, the topics in my array are rendered into buttons and displayed on the top of the screen 
+
+upon clicking each button at the top, the website hits the Giphy api with the relevant search term, pulls 10 gifs, and displays them onscreen into the <div>
+
+upon clicking each gif, it starts and stops playing
+
+upon entering text into the form and clicking submit button, the page throws that into it's own button and renders it onscreen 
+
+when you click the button you made, it pulls 10 gifs and displays them as well as you have the ability to click on them to start/stop
