f4ebfbd119c9d10900b8424848d49c25174edd31
famouswolfe <famouswolfe@gmail.com>
2017-07-29 09:04:21 -0600

    update

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -15,4 +15,67 @@ page pulls each of the strings in the array and creates a button using a for loo
 
 Step 2.) add a button function using jquery $("#heroButtons").on("click", function) etc,
 this sends the name of the button to the Giphy api and it pulls back 10 gifs
-this is accomplished via Ajax call
+this is accomplished via Ajax call 
+*/
+
+$(document).ready(function(){
+
+var heroes = ["Goku", "Superman", "Spiderman", "Batman", "Optimus Prime", "Gandalf"];
+
+//function for displaying buttons
+
+function renderButtons(){
+
+	//must clear div first
+
+	$("#heroButtons").empty();
+
+	for (var i = 0; i < heroes.length; i++) {
+		var a = $("<button>");
+		a.addClass("heroic");
+		a.attr("data-name", heroes[i]);
+		a.text(heroes[i]);
+		$("#heroButtons").append(a);
+	}
+}
+
+//get user input and render buttons upon clicking the submit button
+$("#addHero").on("click", function(event){
+	event.preventDefault();
+
+	var userInput = $("#text-input").val().trim();
+	heroes.push(userInput);
+	renderButtons();
+});
+
+//when you click a hero button it pulls 10 of the relevant gifs from giphy
+//$("#heroButtons").on("click", function(){
+function displayHero(){
+	$("#heroes-appear-here").empty();
+	var hero = $(this).attr("data-name");
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + hero + "&api_key=dc6zaTOxFJmzC&limit=10";
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	})
+	.done(function(response){
+		var results = response.data;
+		for (var j = 0; j < results.length; j++){
+			var heroDiv = $("<div class='heroic'>");
+			var rating = results[j].rating;
+			var p = $("<p>").text("Rating: " + rating);
+			var heroImage = $("<img>");
+			heroImage.attr("src", results[j].images.fixed_height.url);
+			heroDiv.append(p);
+			heroDiv.append(heroImage);
+			$("#heroes-appear-here").prepend(heroDiv);
+			console.log(heroButtons)
+			console.log(heroDiv)
+		}
+	})
+}
+
+$(document).on("click", ".heroic", displayHero);
+
+renderButtons();
+})
